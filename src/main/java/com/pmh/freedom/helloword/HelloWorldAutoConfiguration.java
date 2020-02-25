c11af84d287114814569add460f706c6d760d98a
pmh905001 <pmh905001@126.com>
2020-02-25 12:35:35 +0800

    remove useless code.

--- src/main/java/com/pmh/freedom/helloword/HelloWorldAutoConfiguration.java
+++ src/main/java/com/pmh/freedom/helloword/HelloWorldAutoConfiguration.java
@@ -2,7 +2,6 @@ package com.pmh.freedom.helloword;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
